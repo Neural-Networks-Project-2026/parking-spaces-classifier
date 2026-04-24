8a4c0ac06a0cb5b87184d554480573b2d38ddf09
Kornel Orawczak <marexq123@gmail.com>
2026-04-24 15:50:25 +0200

    dataset i dataloader gotowy, szybkie sprawdzenie czy działa

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 .PHONY: fetch-data
 
 fetch-data:
-	@echo "Pobieranie datasetu PKLot z Kaggle..."
 	mkdir -p data/raw
 	kaggle datasets download -d ammarnassanalhajali/pklot-dataset -p data/raw --unzip
-	@echo "Dane pobrane i rozpakowane w data/raw!"
+	@echo "data are now in data/raw"
---
+++ dataset.py
@@ -0,0 +1,79 @@
+import json
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+from torchvision.transforms import functional as F
+
+class PKLotDataset(Dataset):
+    def __init__(self, root_dir, annotation_file, transforms=None):
+        self.root_dir = root_dir
+        self.transforms = transforms
+        
+        with open(annotation_file, 'r') as f:
+            self.coco = json.load(f)
+
+        self.images = {img['id']: img for img in self.coco['images']}
+        self.img_to_anns = {img['id']: [] for img in self.coco['images']}
+        
+        for ann in self.coco['annotations']:
+            self.img_to_anns[ann['image_id']].append(ann)
+
+        self.image_ids = list(self.images.keys())
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        img_id = self.image_ids[idx]
+        img_info = self.images[img_id]
+
+        img_path = os.path.join(self.root_dir, img_info['file_name'])
+        image = Image.open(img_path).convert("RGB")
+
+        anns = self.img_to_anns[img_id]
+        boxes = []
+        labels = []
+
+        for ann in anns:
+            x_min, y_min, w, h = ann['bbox']
+            boxes.append([x_min, y_min, x_min + w, y_min + h])
+            labels.append(ann['category_id'])
+
+        target = {}
+        target['boxes'] = torch.tensor(boxes, dtype=torch.float32)
+        target['labels'] = torch.tensor(labels, dtype=torch.int64)
+        target['image_id'] = torch.tensor([img_id])
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+        else:
+            image = F.to_tensor(image)
+
+        return image, target
+    
+# Funkcja zapobiegająca błędowi sklejania tensorów o różnym rozmiarze
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+if __name__ == '__main__':
+    train_img_dir = "data/raw/train"
+    train_ann_file = "data/raw/train/_annotations.coco.json"
+
+    train_dataset = PKLotDataset(
+        root_dir=train_img_dir,
+        annotation_file=train_ann_file
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=4, 
+        shuffle=True, 
+        num_workers=2, 
+        collate_fn=collate_fn 
+    )
+
+    for images, targets in train_loader:
+        print(f"Kształt zdjęcia: {images[0].shape}")
+        print(f"Ilość obiektów na pierwszym zdjęciu: {len(targets[0]['labels'])}")
+        break
